88dfb7b1079a3dc6fd1cc4b152778b52a84b0a76
Chi Vang <Chi@tplus.la>
2025-04-18 16:50:52 +0700

    update 4/18 b Chi

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
- module.exports = {
+module.exports = {
     apps: [
-        {
-            name: 'TAXLIST',
-            exec_mode: 'cluster',
-            interpreter: "none",
-            instances: '1', // Or a number of instances
-            script: 'npm',
-            args: 'run dev',
-            // max_memory_restart: "500M"
-        },
-    ],
-}
+      {
+        name: 'TAXLIST',
+        script: 'node_modules/vite/bin/vite.js',
+        args: 'preview --port 7878 --host',
+        exec_mode: 'fork',
+        instances: 1,
+        interpreter: 'node',
+      }
+    ]
+  }
+  
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    host:true
-  }
+    host:true,
+    port: 7878,
+  },
+  preview: {
+    port: 7878,
+  },
 })
