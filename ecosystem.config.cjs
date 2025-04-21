01f6399278909157d754c46b27e54b10e80e466f
Chi Vang <Chi@tplus.la>
2025-04-21 14:47:54 +0700

    update 4/21

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,11 +3,12 @@ module.exports = {
       {
         name: 'TAXLIST',
         script: 'node_modules/vite/bin/vite.js',
-        args: 'preview --port 7878 --host',
+        args: 'preview --host 0.0.0.0 --port 7878',
         exec_mode: 'fork',
         instances: 1,
         interpreter: 'node',
+        // cwd: 'D:/project/ProjectTPlus/React_js/Tplus_Taxlist',
       }
     ]
   }
-  
+   // run: pm2 start ecosystem.config.cjs
